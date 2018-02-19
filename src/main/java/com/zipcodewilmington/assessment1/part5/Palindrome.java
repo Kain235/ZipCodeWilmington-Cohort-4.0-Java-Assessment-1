86daf403c7cab83d7aa14e6bb4050e5455783904
Anthony Jordan <anthonyjordan@zipcoders-MacBook-Pro-10.local>
2018-02-19 16:33:26 -0500

    Finished part 5

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -2,7 +2,18 @@ package com.zipcodewilmington.assessment1.part5;
 
 public class Palindrome {
 
-    public Integer countPalindromes(String input){
-        return null;
+    public Integer countPalindromes(String input) {
+        int palindromeCount = 0;
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i; j <= input.length(); j++) {
+                String subString = input.substring(i, j);
+                StringBuilder revesreString = new StringBuilder(subString);
+                revesreString.reverse();
+                if (subString.equals(revesreString.toString()) && !(subString.equals(""))) {
+                    palindromeCount++;
+                }
+            }
+        }
+        return palindromeCount;
     }
 }
